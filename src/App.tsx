276b873b0073e681182e37abcf3d83f8a490abb3
Sabry Awad <dr.sabry1997@gmail.com>
2023-11-01 16:55:44 +0200

    ✨ feat(App.tsx): Add input field and submit button to the UI 🔄 refactor(App.tsx): Refactor code to handle input and display output

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
+import { useState } from "react";
 import "./App.css";
 
 const App = () => {
+  const [input, setInput] = useState("const a = 1;");
+  const [code, setCode] = useState("");
+
+  const handleClick = () => {
+    console.log(input);
+  };
+
   return (
     <div>
-      <h1>Welcome to Tauri!</h1>
+      <textarea
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      ></textarea>
+      <div>
+        <button onClick={handleClick}>Submit</button>
+      </div>
+      <pre>{code}</pre>
     </div>
   );
 };

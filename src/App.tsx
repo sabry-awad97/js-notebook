235c01bddb02cd45549668436fae1fe4cefe5103
Sabry Awad <dr.sabry1997@gmail.com>
2023-11-01 19:07:56 +0200

    ✨ feat(package.json): Add "@preact/signals-react" dependency 📦 chore(public/esbuild.wasm): Add esbuild.wasm binary 🔄 refactor(src/App.tsx): Refactor code transformation and initialization logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,51 @@
-import { useState } from "react";
-import "./App.css";
+import * as esbuild from 'esbuild-wasm';
+import { useEffect, useState } from 'react';
+import './App.css';
 
 const App = () => {
-  const [input, setInput] = useState("const a = 1;");
-  const [code, setCode] = useState("");
+  const [input, setInput] = useState('const a = 1;');
+  const [code, setCode] = useState('');
 
-  const handleClick = () => {
-    console.log(input);
+  const [initialized, setInitialized] = useState(false);
+
+  useEffect(() => {
+    const initializeEsbuild = async () => {
+      if (!initialized) {
+        await esbuild.initialize({
+          worker: true,
+          wasmURL: '/esbuild.wasm',
+        });
+
+        setInitialized(true);
+      }
+    };
+
+    initializeEsbuild().catch(() => {});
+  }, [initialized]);
+
+  const transformCode = async (input: string): Promise<string> => {
+    if (!initialized) {
+      return '';
+    }
+
+    const result = await esbuild.transform(input, {
+      loader: 'jsx',
+      target: 'es2015',
+    });
+
+    return result.code;
+  };
+
+  const handleClick = async () => {
+    const code = await transformCode(input);
+    setCode(code);
   };
 
   return (
     <div>
       <textarea
         value={input}
-        onChange={(e) => setInput(e.target.value)}
+        onChange={e => setInput(e.target.value)}
       ></textarea>
       <div>
         <button onClick={handleClick}>Submit</button>

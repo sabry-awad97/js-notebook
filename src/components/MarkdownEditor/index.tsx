acb557d78bcd6db1410d393006922c71e7e28dce
Sabry Awad <dr.sabry1997@gmail.com>
2023-11-02 17:12:45 +0200

    🔨 refactor(MarkdownEditor): Improve document click handling 🚀 feat(MarkdownEditor): Add click event listener for outside editor clicks

--- src/components/MarkdownEditor/index.tsx
+++ src/components/MarkdownEditor/index.tsx
@@ -1,14 +1,30 @@
 import MDEditor from '@uiw/react-md-editor';
-import { useState } from 'react';
 import './md-editor.css';
+import { useCallback, useRef, useState, ElementRef } from 'react';
+import { useEventListener } from '../../hooks/useEventListener';
 
 const MarkdownEditor: React.FC<{}> = ({}) => {
   const [editing, setEditing] = useState(false);
+  const documentRef = useRef(document);
+  const editorRef = useRef<ElementRef<'div'>>(null);
+
+  const handleClickOutside = useCallback((event: MouseEvent) => {
+    if (
+      event.target instanceof Node &&
+      !editorRef.current?.contains(event.target)
+    ) {
+      setEditing(false);
+    }
+  }, []);
+
+  useEventListener('click', handleClickOutside, documentRef, {
+    capture: true,
+  });
 
   return (
     <div>
       {editing ? (
-        <div>
+        <div ref={editorRef}>
           <MDEditor />
         </div>
       ) : (

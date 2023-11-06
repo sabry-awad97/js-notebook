861347eb27673805b92fa430135a78036beea595
Sabry Awad <dr.sabry1997@gmail.com>
2023-11-06 16:04:13 +0200

    🔍 chore(useCumulativeCode.ts): Improve library deduplication logic 🔄 refactor(useCumulativeCode.ts): Refactor code content handling

--- src/hooks/useCumulativeCode.ts
+++ src/hooks/useCumulativeCode.ts
@@ -14,13 +14,30 @@ const useCumulativeCode = (currentCell: Cell) => {
 
   const cellContent = useMemo(() => {
     const cumulativeCode = [];
+    const importRegex =
+      /import(?:(?:(?:[ \n\t]+(?<defaultImport>[^ *\n\t\{\},]+)[ \n\t]*(?:,|[ \n\t]+))?([ \n\t]*\{(?:[ \n\t]*(?<namedImports>[^ \n\t"'\{\}]+)[ \n\t]*,?)+\})?[ \n\t]*)|[ \n\t]*\*[ \n\t]*as[ \n\t]+(?<namespaceImport>[^ \n\t\{\}]+)[ \n\t]+)from[ \n\t]*(?:['"])(?<importPath>[^'"\n]+)(['"])/g;
+
+    const importedLibraries = new Set<string>();
+
     for (let c of orderedCells) {
       if (c.type === 'code') {
+        const libraries = c.content.match(importRegex);
+
+        // Check if library is not already imported
+        if (libraries) {
+          for (const library of libraries) {
+            if (!importedLibraries.has(library)) {
+              cumulativeCode.push(library);
+              importedLibraries.add(library);
+            }
+          }
+        }
+
         if (c.id === currentCell.id) {
           cumulativeCode.push(`
             import _React from 'react';
             import _ReactDOM from 'react-dom/client';
-            
+
             var show = (value) => {
               const rootElement = document.querySelector('#root');
               const root = _ReactDOM.createRoot(rootElement);
@@ -39,7 +56,10 @@ const useCumulativeCode = (currentCell: Cell) => {
         } else {
           cumulativeCode.push('var show = () => {}');
         }
-        cumulativeCode.push(c.content);
+
+        // Push content without libraries to avoid duplication
+        const contentWithoutLibraries = c.content.replace(importRegex, '');
+        cumulativeCode.push(contentWithoutLibraries);
       }
 
       if (c.id === currentCell.id) {
